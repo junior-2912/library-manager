54afc920f6daa407cef6adfc7d7ee371c964a6a7
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-06-29 12:36:15 -0300

    feat: Modelo inicial da classe emprestimo implementado

---
+++ src/entities/Emprestimo.java
@@ -0,0 +1,41 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class Emprestimo {
+    private Usuario usuario;
+    private Livro livro;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucao;
+    private int id;
+    
+    public Emprestimo(Usuario usuario, Livro livro, LocalDate dataEmprestimo, LocalDate dataDevolucao, int id) {
+        this.usuario = usuario;
+        this.livro = livro;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public LocalDate getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    
+}
