be41b55fc3f4ccdf025d5f0188731860cb9b453b
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-06-30 17:02:45 -0300

    feat: Adicionado toString, equals e hashCode

--- src/entities/Emprestimo.java
+++ src/entities/Emprestimo.java
@@ -1,6 +1,7 @@
 package entities;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Emprestimo {
     private Usuario usuario;
@@ -8,11 +9,11 @@ public class Emprestimo {
     private LocalDate dataEmprestimo;
     private LocalDate dataDevolucao;
     private int id;
-    
-    public Emprestimo(Usuario usuario, Livro livro, LocalDate dataEmprestimo, LocalDate dataDevolucao, int id) {
+
+    public Emprestimo(Usuario usuario, Livro livro, LocalDate dataDevolucao, int id) {
         this.usuario = usuario;
         this.livro = livro;
-        this.dataEmprestimo = dataEmprestimo;
+        this.dataEmprestimo = LocalDate.now();
         this.dataDevolucao = dataDevolucao;
         this.id = id;
     }
@@ -37,5 +38,23 @@ public class Emprestimo {
         return id;
     }
 
-    
+    @Override
+    public String toString() {
+        return id + "\n " + usuario + " - " + livro + "\n"
+                + "Data de emprestimo: " + dataEmprestimo.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+                + ". Data de devolucao: " + dataDevolucao.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Emprestimo that = (Emprestimo) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
